5ef447b7cf8bfdffcc3baed7b462b22b958bce55
Nguyen Van Hiep <hiep.5.5.1999@gmail.com>
2022-06-27 01:41:07 +0700

    kết nối tới database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import viewEngine from './config/viewEngine'
 import initWebRoutes from './route/web'
+import connectDB from './config/connectDB'
 require('dotenv').config()
 
 let app = express();
@@ -11,10 +12,16 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB()
+
 let port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`be nodejs is running on the port: ${port}`);
 }
 );
-viewEngine(app);
-initWebRoutes(app);
